b7780316c0b98cf0f1c80e3721d3222a77f95c11
Nick Snyder <nsnyder1992@gmail.com>
2021-03-11 18:12:20 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//environment
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
