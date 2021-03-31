67873a88d41262d0fbe6f162863250b5791ccb00
Nick Snyder <nsnyder1992@gmail.com>
2021-03-31 07:26:37 -0400

    added about model

--- models/about.js
+++ models/about.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    image: {
+    photoUrl: {
       type: DataTypes.STRING,
       allowNull: false,
     },
