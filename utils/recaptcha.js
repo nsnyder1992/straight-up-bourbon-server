144d4c92000406111e56acece39e8174d197c76f
Nick Snyder <nicholas.snyder@accruent.com>
2022-09-26 18:19:15 -0500

    fixing recaptcha 3

---
+++ utils/recaptcha.js
@@ -0,0 +1,20 @@
+require("dotenv");
+const request = require("request-promise");
+
+const isRobot = async (token) => {
+  try {
+    const response = await request({
+      url: `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+      method: "POST",
+      resolveWithFullResponse: true,
+    });
+    console.log(response);
+
+    return response.statusCode != 200;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+exports.trackPackage = trackPackage;
